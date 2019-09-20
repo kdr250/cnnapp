96c3b18aef0db534d1ad8a6d0003781644c2cc51
kdr250 <ytkodera4863@gmail.com>
2019-09-20 20:31:56 +0900

    small-fix8
    small-fix8

--- cnnapp/settings.py
+++ cnnapp/settings.py
@@ -144,9 +144,9 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
