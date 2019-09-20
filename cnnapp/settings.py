40b923e7e5a67353cc9a8f3a196445d3ce8e1132
kdr250 <ytkodera4863@gmail.com>
2019-09-20 18:56:41 +0900

    static-file-short-fix
    static-file-short-fix

--- cnnapp/settings.py
+++ cnnapp/settings.py
@@ -128,7 +128,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
