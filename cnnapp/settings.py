966a3db86a2e8e2f9bd4890328a05b0883937eef
kdr250 <ytkodera4863@gmail.com>
2019-09-20 20:20:59 +0900

    change-settings-of-db
    change-settings-of-db

--- cnnapp/settings.py
+++ cnnapp/settings.py
@@ -95,13 +95,16 @@ WSGI_APPLICATION = 'cnnapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cnnapp', # データベースを作成してください
-        'USER': 'root', # ログインユーザー名
-        'HOST': 'localhost',
-        'PORT': '5432',
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3",
     }
 }
+
+# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cnnapp', # データベースを作成してください
+#         'USER': 'root', # ログインユーザー名
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
