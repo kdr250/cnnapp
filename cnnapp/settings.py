11908865c5f4cdf198ab90e0c6f365bd80cb4040
kdr250 <ytkodera4863@gmail.com>
2019-11-01 15:07:23 +0900

    S3設定追加4

--- cnnapp/settings.py
+++ cnnapp/settings.py
@@ -112,9 +112,7 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
